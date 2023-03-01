df2f55da6a761e848a35de7bfe64c7a688be3883
AERIM CHOI <80438964+aerim-choi@users.noreply.github.com>
2023-03-02 01:27:03 +0900

    chapter7.Hook실습코드

--- src/index.js
+++ src/index.js
@@ -8,12 +8,13 @@ import reportWebVitals from './reportWebVitals';
 
 import CommentList from './chapter_05/CommentList';
 import NotificationList from './chapter_06/NotificationList';
+import Accomodate from './chapter_07/Accommodate'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 
 root.render(
   <React.StrictMode>
-    <NotificationList />
+    <Accomodate />
   </React.StrictMode>,
 );
 
